0c0e9f7c705167f15ba388d91abdcf772b1fe9b0
hemags <g.s.hemanthbabu@gmail.com>
2016-11-07 07:28:19 +0000

    initiated web development

--- python/python_scripts/generate_ps_json.py
+++ python/python_scripts/generate_ps_json.py
@@ -4,6 +4,7 @@ import subprocess
 import sys
 import re
 import time
+import datetime
 
 def pid_json(pid):
 	try:
@@ -24,7 +25,7 @@ def pid_json(pid):
 def ps_rollin():
 	c_time=time.strftime("%c")
 	fobj_log.write('\n\n\n ================================================= \n 	%s \n =================================================\n'%c_time)
-	fobj_log.write('\n Retrieving all the process data .....')
+	fobj_log.write('\n %s ==> Retrieving all the process data .....'%(datetime.datetime.today()))
 	ps='ps -ef'
 	psoc= subprocess.Popen(ps, stdout=subprocess.PIPE,shell=True)
 	output=psoc.stdout.read()
@@ -32,35 +33,30 @@ def ps_rollin():
 	#output=re.sub('\n',' \n ',output)
 	alist=output.split('\n')
 	length=len(alist)
-	fobj_log.write('\n Wrapping Processes Data into json .....')
+	fobj_log.write('\n %s ==> Wrapping Processes Data into json .....'%(datetime.datetime.today()))
 	del alist[length-1]
 	del alist[0]
-	fobj_log.write('\n Eliminating unnecessary json lists .....')
+	fobj_log.write('\n %s ==> Eliminating unnecessary json lists .....'%(datetime.datetime.today()))
 	pid_data={'data':[]}
-	fobj_log.write('\n Appending CPU and RAM data of each process to json string .....')
+	fobj_log.write('\n %s ==> Appending CPU and RAM data of each process to json string .....'%(datetime.datetime.today()))
 	for list in alist:
 		li_output=list.split(' ',6)
 		li_json={'User-Id':li_output[0],'PID':li_output[1],'Parent_PID':li_output[2],'STARTUP_TIME':li_output[4]}
 		pid_json(li_output[1])
 		li_json.update(dict1)
 		pid_data['data'].append(li_json)
-	fobj_json_raw=open('/vagrant_data/data_files/raw.json','wb')
+	fobj_json_raw=open('../data_files/raw.json','wb')
 	fobj_json_raw.write("%s"%pid_data)
 	fobj_json_raw.close()
-	fobj_log.write('json data has been generated at /vagrant_data/data_files/raw.json ')
-	#pid={'data':[]}
-	
-	
-	
-	#print pid_data
-	#ram=0.0
-	#for d in pid_data['data']:
-	#	tmp=d['RAM']
-	#	ram=ram+float(tmp)
-	#print ram
+	fobj_log.write('\n %s ==> JSON data has been generated at /vagrant_data/data_files/raw.json .....'%(datetime.datetime.today()))
 
+	ram=0.0
+	for d in pid_data['data']:
+		tmp = d['RAM']
+		ram = ram + float(tmp)
+	print ram
 
 global fobj_log
-fobj_log=open('/vagrant_data/logs/generate.log','a+')
+fobj_log=open('../logs/generate.log','a+')
 ps_rollin()
 fobj_log.close()
